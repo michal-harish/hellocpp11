6789310ee24e7a0982f00f18b7135fea8c189f7a
mharis <michal.harish@gmail.com>
2016-07-25 21:10:59 +0100

    basic autoconf

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#if !defined(__cplusplus)
+#error C++ compiler required.
+#endif
+
+#if __cplusplus > 199711L
+#ifndef HAS_MOVE_SEMANTICS
+#error compiler lies - its at least C++11 but not support move semantics 
+#endif
+#else
+#error at least C++11 compiler required
+#endif 
+
 #include <iostream>
 #include <vector>
 
@@ -8,6 +20,8 @@
 using namespace std;
 
 int main() {
+
+	cout << __cplusplus;
 
 	vector<int> v { 1,2,3,4,5,6, myfunc() };
 
